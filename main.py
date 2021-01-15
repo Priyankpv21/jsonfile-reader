e3408bcbd33d9ec506f7dc446be95b3c60c98c7f
Priyankpv21 <77071465+Priyankpv21@users.noreply.github.com>
2021-01-15 15:51:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 from lib import utils
 
 parser = argparse.ArgumentParser(description='\nThis program Read JSON file... ')
@@ -6,4 +7,4 @@ parser.add_argument('file name',help='Add JSON file name')
 args = parser.parse_args()
 file_name =  getattr(args,'file name')
 json_data = utils.read_json(file_name)
-print(json_data)
+print(json.dumps(json_data, indent=4, sort_keys=True))
